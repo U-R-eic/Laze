aa5bff64a3740a5a53294ac381d62ed5dfe402b6
U-R-eic <richard.ullmann.09@outlook.de>
2025-03-18 18:34:29 +0100

    update dates

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Calendar Data
 const upcomingVideos = [
-    { date: '2024-03-19', title: 'Wie werde ich besser in Fortnite', type: 'Fortnite', save: 'höchst warscheinich'},
-    { date: '2024-03-23', title: 'Minecraft Hardcore: Der gefährlichste erste Tag!', type: 'Minecraft', save: 'vermutlich'},
-    { date: '2024-03-28', title: 'Fortnite Mythos: Ist das wirklich möglich?!', type: 'Fortnite', save: 'vermutlich'},
-    { date: '2024-03-31', title: 'Minecraft Osterevent: Das geheime Ei?', type: 'Minecraft', save: 'vermutlich'},
-    { date: '2024-04-03', title: 'Fortnite: Die krasseste Runde EVER?!', type: 'Fortnite', save: 'vermutlich'},
-    { date: '2024-04-06', title: 'Minecraft 100 Days: So OP war ich noch nie!', type: 'Minecraft', save: 'vermutlich'},
-    { date: '2024-04-09', title: 'Fortnite vs. Minecraft: Wer ist der bessere Speedrunner?', type: 'Mix', save: 'vermutlich'},
+    { date: '2025-03-19', title: 'Wie werde ich besser in Fortnite', type: 'Fortnite', save: 'höchst warscheinich'},
+    { date: '2025-03-23', title: 'Minecraft Hardcore: Der gefährlichste erste Tag!', type: 'Minecraft', save: 'vermutlich'},
+    { date: '2025-03-28', title: 'Fortnite Mythos: Ist das wirklich möglich?!', type: 'Fortnite', save: 'vermutlich'},
+    { date: '2025-03-31', title: 'Minecraft Osterevent: Das geheime Ei?', type: 'Minecraft', save: 'vermutlich'},
+    { date: '2025-04-03', title: 'Fortnite: Die krasseste Runde EVER?!', type: 'Fortnite', save: 'vermutlich'},
+    { date: '2025-04-06', title: 'Minecraft 100 Days: So OP war ich noch nie!', type: 'Minecraft', save: 'vermutlich'},
+    { date: '2025-04-09', title: 'Fortnite vs. Minecraft: Wer ist der bessere Speedrunner?', type: 'Mix', save: 'vermutlich'},
 ];
 
 
